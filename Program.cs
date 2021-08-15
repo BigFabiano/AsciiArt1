9446d9b92bba5bceaea982d91d67e431faea9ec2
FabianoEtec <nbscbs@gmail.com>
2021-08-15 15:18:57 -0300

    projeto finalizado

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AsciiArt1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ||  || ");
+            Console.WriteLine(" \\\\()// ");
+            Console.WriteLine("//(__)\\\\");
+            Console.WriteLine("||    ||");
+        }
+    }
+}
